22fca87183c04a46c7c584074bcbe2b764b66f8d
K3vin135 <123042020+K3vin135@users.noreply.github.com>
2026-05-08 18:36:14 -0500

    Create bouquet_monitor.ino

---
+++ examples/bouquet_monitor/bouquet_monitor.ino
@@ -0,0 +1,38 @@
+#include <HavatecIoT.h>
+
+HavatecIoT iot(
+  "SSID",
+  "SSID_PASSWORD",
+  "xxx.azure-devices.net",
+  "deviceName",
+  "deviceprimarykey"
+);
+
+void setup() {
+  Serial.begin(115200);
+  iot.begin();
+}
+
+void loop() {
+  iot.loop();
+
+  // Marca ANTES de publicar
+  unsigned long antes = micros();
+
+  iot.publish(Payload()
+    .add("estado", "ON")
+    .add("bunches", 42)
+  );
+
+  // Marca DESPUÉS de publicar
+  unsigned long despues = micros();
+
+  unsigned long duracion_us = despues - antes;
+  float duracion_ms = duracion_us / 1000.0;
+
+  Serial.print("Tiempo publish(): ");
+  Serial.print(duracion_ms);
+  Serial.println(" ms");
+
+  delay(10000);
+}
